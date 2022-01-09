5ef5e790bc9955069678c20bbd62e67b36a6970b
mayank <44097941+codr-mayank@users.noreply.github.com>
2022-01-09 21:43:47 +0530

    babel and webpack configs and setup

---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  optimization: {
+    splitChunks: {
+      minSize: 10000,
+      maxSize: 250000
+    }
+  }
+});
