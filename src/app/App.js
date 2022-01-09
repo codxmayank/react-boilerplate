92e2089d18cb52b8228ed9d63265bad55f1bc951
mayank <44097941+codr-mayank@users.noreply.github.com>
2022-01-09 22:35:24 +0530

    test husky

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="helloWorld">{`${displayLine} ${displayName}`}</h1>
+      <h1 className="helloWorld">{`${displayLine} ${displayName}`}!</h1>
     </div>
   );
 };
