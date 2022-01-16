5080fbf6cc6817e9b02ad87a20a2c49544a04a18
mayank <44097941+codr-mayank@users.noreply.github.com>
2022-01-16 19:00:35 +0530

    fix mini css extra plugin issue

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin').default;
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
